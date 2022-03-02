b3856618dcc01bbbf7b3e79b0a1420430e8c1398
Jason Bock <jasonbock1@rocketmortgage.com>
2022-03-02 14:19:07 -0600

    Minor demo additions

---
+++ UsingAnalyzers/UsingArrayForProperty.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace UsingAnalyzers
+{
+	public sealed class UsingArrayForProperty
+	{
+		public string[] Values { get; set; }
+
+		public IEnumerable<string> GetData()
+		{
+			var data = new List<string>();
+			data.Add("a");
+			return data;
+		}
+	}
+}
