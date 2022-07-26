a28b90e87cc2bc6fb876e30a71c0b0b051a19479
Jason Bock <jason.r.bock@outlook.com>
2022-07-26 09:50:40 -0500

    Added a NUnit analyzer example

---
+++ NUnitAnalyzerDemo/ParameterizedTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace NUnitAnalyzerDemo;
+
+public static class ParameterizedTests
+{
+	[TestCase(3, 4, 7)]
+	public static void Add(int x, int y, int expectedResult) => 
+		Assert.That(x + y, Is.EqualTo(expectedResult));
+}
